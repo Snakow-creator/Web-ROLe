76029be8946ee8eb8a627900ca03c325100fb622
Snakow-creator <mark.politov20070@gmail.com>
2026-01-10 16:48:54 +0300

    deploy mode added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,21 +3,25 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  // api remover if /api in url, return 0.0.0.0:7878
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:7878',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '') // remove /api
-      }
-    },
-    host: "127.0.0.1",
-    port: 5173
+export default defineConfig(({mode}) => {
+    const isCI = mode === 'ci'
+    return {
+    base: isCI ? '/ROLe/' : '/',
+    plugins: [
+      react(),
+      tailwindcss(),
+    ],
+    // api remover if /api in url, return 0.0.0.0:7878
+    server: {
+      proxy: {
+        '/api': {
+          target: 'http://localhost:7878',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '') // remove /api
+        }
+      },
+      host: "127.0.0.1",
+      port: 5173
+    }
   }
 })
